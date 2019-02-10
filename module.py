156adf8204abd61b023ae27c69f605d1c3c9c223
Joseph Haaga <haaga.joe@gmail.com>
2019-02-09 19:32:44 -0500

    restructure directory and implement GetStructuralSignatures()

--- module.py
+++ module.py
@@ -7,9 +7,6 @@ import pandas as pd
 import numpy as np
 from graphwave import graphwave
 from graphwave.utils import utils
-from sklearn.decomposition import PCA
-from sklearn.preprocessing import StandardScaler
-from sklearn.cluster import KMeans
 
 def GetStructuralSignatures(networkXGraph):
     """
@@ -25,39 +22,7 @@ def GetStructuralSignatures(networkXGraph):
     Returns: a NetworkX graph where structural embeddings are added as `structure` node attributes.
 
     """
-    nb_clust = 6
-    trans_data_all = []
-    n_components = 4
-    keys = []
-    nodes_list = []
-
-    components = list(set(nx.get_node_attributes(G=networkXGraph, name='component').values()))
-
-    for id in components:
-        subgraph = G.subgraph(id)
-        chi, heat_print, taus = graphwave.graphwave_alg(
-            subgraph, np.linspace(0, 100, 25), taus='auto', verbose=True)
-        if len(subgraph.nodes) < n_components:
-            print("Omitting graph " + key + " with node count: " +
-                str(len(subgraph.nodes)) + " < " + n_components)
-        else:
-            pca = PCA(n_components)
-            trans_data = pca.fit_transform(StandardScaler().fit_transform(chi))
-            trans_data_all = trans_data_all + trans_data.tolist()
-            l = [key] * len(subgraph.nodes)
-            keys = keys + l
-            nodes_list = nodes_list + list(subgraph.nodes())
-
-    km = KMeans(n_clusters=nb_clust)
-    km.fit(trans_data_all)
-    labels_pred = km.labels_
-
-    out = pd.DataFrame(labels_pred)
-    out.index += 1
-    structure_labels = out[0].to_dict()
-    nx.set_node_attributes(G=networkXGraph, values=structure_labels, name='structure')
-
-    return networkXGraph
+    return True
 
 def WalkAsString(networkXGraph, graphComponentLabels, featuresToOmit={"nodes":[], "edges":[]}):
     """
