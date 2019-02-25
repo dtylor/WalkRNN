0fa2ae155fa0593abcb60b67c9259d318efa1909
Joseph Haaga <haaga.joe@gmail.com>
2019-02-25 09:27:33 -0500

    all tests passing

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import unittest
 import networkx as nx
 from functools import reduce
 
-class TestLoadGraph(unittest.TestCase):
+class TestUtilityFunctions(unittest.TestCase):
     def setUp(self):
         mappings =  {
             "node_labels": [{
@@ -65,59 +65,29 @@ class TestLoadGraph(unittest.TestCase):
             len(attr_1), 0, 'loaded Graph edges have no "attr_1" attributes')
         return True
 
-class TestGetStructuralSignatures(unittest.TestCase):
+class TestModuleFunctions(unittest.TestCase):
     def setUp(self):
         mappings = {
             "node_labels": [{
-                0:       "C",
-                1:       "O",
-                2:       "N",
-                3:       "Cl",
-                4:       "F",
-                5:       "S",
-                6:       "Se",
-                7:       "P",
-                8:       "Na",
-                9:       "I",
-                10:      "Co",
-                11:      "Br",
-                12:      "Li",
-                13:      "Si",
-                14:      "Mg",
-                15:      "Cu",
-                16:      "As",
-                17:      "B",
-                18:      "Pt",
-                19:      "Ru",
-                20:      "K",
-                21:      "Pd",
-                22:      "Au",
-                23:      "Te",
-                24:      "W",
-                25:      "Rh",
-                26:      "Zn",
-                27:      "Bi",
-                28:      "Pb",
-                29:      "Ge",
-                30:      "Sb",
-                31:      "Sn",
-                32:      "Ga",
-                33:      "Hg",
-                34:      "Ho",
-                35:      "Tl",
-                36:      "Ni",
-                37:      "Tb"
+                0:  "depthPoint",
+                1:	"tailVertex",
+                2:	"leftVertex",
+                3:	"rightVertex",
+            }, {
+                0:	"vertical",
+                1:	"Winkelhaken",
+                2:	"horizontal"
             }],
             "edge_labels": {
-                0: 1,
-                1: 2,
-                2: 3
+                0:	"wedge",
+                1:	"arrangement"
             },
-            "node_attributes": ["chem", "charge", "x", "y"]
         }
-        self.graph = load_graph_kernel_graph("./AIDS", mappings=mappings)
+        self.graph = load_graph_kernel_graph("./Cuneiform", mappings=mappings)
+        self.y = load_graph_kernel_labels("./Cuneiform")
         self.graph, self.pca, self.km = get_structural_signatures(self.graph)
-    
+        self.walks = walk_as_string(self.graph, self.y)
+
     def test_structural_signatures_assigned(self):
         structs = nx.get_node_attributes(self.graph, 'structure')
         structs = list(set(structs.values()))
@@ -125,64 +95,10 @@ class TestGetStructuralSignatures(unittest.TestCase):
             len(structs), 1, 'structures not assigned as node attributes, or are homogeneous')
         return True
 
-class TestWalkAsString(unittest.TestCase):
-    def setUp(self):
-        mappings = {
-            "node_labels": [{
-                0:       "C",
-                1:       "O",
-                2:       "N",
-                3:       "Cl",
-                4:       "F",
-                5:       "S",
-                6:       "Se",
-                7:       "P",
-                8:       "Na",
-                9:       "I",
-                10:      "Co",
-                11:      "Br",
-                12:      "Li",
-                13:      "Si",
-                14:      "Mg",
-                15:      "Cu",
-                16:      "As",
-                17:      "B",
-                18:      "Pt",
-                19:      "Ru",
-                20:      "K",
-                21:      "Pd",
-                22:      "Au",
-                23:      "Te",
-                24:      "W",
-                25:      "Rh",
-                26:      "Zn",
-                27:      "Bi",
-                28:      "Pb",
-                29:      "Ge",
-                30:      "Sb",
-                31:      "Sn",
-                32:      "Ga",
-                33:      "Hg",
-                34:      "Ho",
-                35:      "Tl",
-                36:      "Ni",
-                37:      "Tb"
-            }],
-            "edge_labels": {
-                0: "edge_1",
-                1: "edge_2",
-                2: "edge_3"
-            },
-            "node_attributes": ["chem", "charge", "x", "y"]
-        }
-        self.graph = load_graph_kernel_graph("./AIDS", mappings=mappings)
-        self.graph, self.pca, self.km = get_structural_signatures(self.graph)
-        self.y = load_graph_kernel_labels("./AIDS")
-        self.walks = walk_as_string(self.graph, self.y)
-
     def test_valid_words(self):
         for walk in self.walks['walk']:
             w = walk.split(" ")
             for step in w:
-                self.assertTrue(len(step.split("_")) > 1, 'word "' + step + '" does not contain underscore')
+                self.assertTrue(len(step.split("_")) > 1, 'word "' +
+                                step + '" does not contain underscore')
 
